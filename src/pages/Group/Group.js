eb7cc4574d0a06fdec146ec6f767fe821917ece1
mongo <sihamongkul123@gmail.com>
2024-01-11 18:58:44 +1100

    rando

--- src/pages/Group/Group.js
+++ src/pages/Group/Group.js
@@ -3,11 +3,11 @@ import Sidebar from '../../components/Sidebar/Sidebar.js';
 
 const GroupPage = () => {
     return (
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 ">
             <div className="bg-pink-400 col-span-1">
                 <Sidebar />
             </div>
-            <div className="col-span-3 grid grid-cols-3 bg-green-400">
+            <div className="col-span-3 grid grid-cols-3 bg-green-400 h-screen">
                 <h1 className="col-start-2 col-end-2">
                     Group
                 </h1>
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,21 +1,23 @@
 import React from "react";
 import Sidebar from '../../components/Sidebar/Sidebar.js';
 import { Link } from 'react-router-dom';
-
+import "./HomePage.css";
 const HomePage = () => {
     return (
-        <div className="bg-orange-500">
+        <div>
             <div className="Sidebar"></div>
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 h-screen">
             <div className="bg-pink-400 col-span-1">
                 <Sidebar />
             </div>
             <div className="col-span-3 bg-blue-400">
                 <div id="trending">
                     <h1>TRENDING</h1>
+
                 </div>
                 <div id="mood-mixer">
                     <h1>MOOD MIXER</h1>
+
                 </div>
                 <div id="join-group">
                     <Link to='/group'>JOIN GROUP</Link>
