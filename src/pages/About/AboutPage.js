a2bb52e0137b0c4268f795ffad50a40bed09ee30
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-11 02:41:20 -0500

    finish about page

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -7,10 +7,17 @@ const AboutPage = () => {
             <div className="bg-pink-400 col-span-1">
                 <Sidebar />
             </div>
-            <div className="col-span-5 bg-yellow-400 h-screen">
-                <h1 className="col-start-2 col-end-2">
+            <div className=" col-span-5 bg-yellow-400 h-screen flex flex-col items-center gap-8">
+                <h1 className="col-span-3 col-start-2 text-6xl my-3">
                     About Us!
                 </h1>
+                <div className="">
+                    <p className="text-lg">
+                        Too often when I want watch movies I question myself, <i>I don't know what to watch</i>. Then I would go to those top 5 or top 10 movie sites and look from their list<br></br>
+                        But I would never feel satisfied with the selection, after which I proceed to go ask my friends for recommendations. I wished there was a website that does this. <br></br>
+                        <b>So I made it</b>.
+                    </p>
+                </div>
             </div>
         </div>
     )
