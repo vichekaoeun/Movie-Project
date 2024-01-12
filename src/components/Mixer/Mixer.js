0234f5b741f043049f610c00f1f5ae169bbd96c5
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-11 21:31:32 -0500

    add randomized button generator

--- src/components/Mixer/Mixer.js
+++ src/components/Mixer/Mixer.js
@@ -1,22 +1,52 @@
 import React from "react";
 import './Mixer.css';
 
+// Generate a random size value
+const getRandomSize = () => {
+    const minSize = 50;
+    const maxSize = 200;
+    return Math.floor(Math.random() * (maxSize - minSize + 1)) + minSize;
+};
+
+// Generate a random position
+const getRandomPosition = () => {
+    const maxPosition = 300;
+    return Math.floor(Math.random() * maxPosition);
+};
+
 const Mixer = () => {
+    const numButtons = 5;
+    const buttons = []; // Array to store buttons
+
+    for (let i = 0; i < numButtons; i++) {
+        const buttonStyles = {
+            width: getRandomSize(),
+            height: getRandomSize(),
+            left: getRandomPosition(),
+            top: getRandomPosition(),
+        }
+        console.log(buttonStyles);
+
+        buttons.push(
+            <button key={i} style={buttonStyles}>
+                <svg width="100%" height="100%" viewBox="0 0 352 331" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="Frame 1">
+                        <g id="darkGroup">
+                            <ellipse id="dark1" cx="176.5" cy="151.5" rx="133.5" ry="62.5" />
+                        </g>
+                        <g id="lightGroup">
+                            <ellipse id="light1" cx="176.5" cy="151.5" rx="133.5" ry="62.5" />
+                        </g>
+                    </g>
+                </svg>
+            </button>
+        )
+    }
+
     return (
         <>
             <div className="mixer-container">
-                <button>
-                    <svg width="352" height="331" viewBox="0 0 352 331" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="Frame 1">
-                            <g id="darkGroup">
-                                <ellipse id="dark1" cx="176.5" cy="151.5" rx="133.5" ry="62.5" />
-                            </g>
-                            <g id="lightGroup">
-                                <ellipse id="light1" cx="176.5" cy="151.5" rx="133.5" ry="62.5" />
-                            </g>
-                        </g>
-                    </svg>
-                </button>
+                {buttons}
             </div>
         </>
     )
