4bafb20866be932af3428053adbf5594b4d11857
mongo <sihamongkul123@gmail.com>
2024-01-13 19:10:00 +1100

    added description for Mood Mixer

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -6,7 +6,7 @@ import "./HomePage.css";
 const HomePage = () => {
     return (
         <div>
-            <div className="grid grid-cols-6 h-screen bg-gradient-to-tr from-yellow-600 to-purple-600">
+            <div className="grid grid-cols-6 bg-gradient-to-tr from-yellow-600 to-purple-600">
                 <div className="bg-purple-500 bg-opacity-30 col-span-1">
                     <Sidebar />
                 </div>
@@ -17,6 +17,7 @@ const HomePage = () => {
                     <div id="mood-mixer">
                         <div>
                             <h1>MOOD MIXER</h1>
+                            <p id = "parag">To discover the movies your heart desires, please select the mood you are currently feeling:</p> 
                         </div>
                         <div className="w-100 h-100 flex items-center flex-col">
                             < Mixer />
