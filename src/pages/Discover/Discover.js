c861109d63e9d451a4157a2da188d05449268d35
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-14 22:54:58 -0500

    add development tag to unfinished section

--- src/pages/Discover/Discover.js
+++ src/pages/Discover/Discover.js
@@ -4,12 +4,12 @@ import Sidebar from '../../components/Sidebar/Sidebar.js';
 const DiscoverPage = () => {
     return (
         <div className="grid grid-cols-6 h-screen bg-gradient-to-tr from-yellow-600 to-purple-600">
-        <div className="bg-purple-500 bg-opacity-30 col-span-1">
-            <Sidebar />
-        </div>
-            <div className="col-span-5 h-screen">
-                <h1 className="col-start-2 col-end-2">
-                    Discover!
+            <div className="bg-purple-500 bg-opacity-30 col-span-1">
+                <Sidebar />
+            </div>
+            <div className="col-span-5 h-screen flex justify-center mt-5">
+                <h1 className="col-start-2 col-end-2 text-5xl text-slate-100">
+                    Under Development.
                 </h1>
             </div>
         </div>
