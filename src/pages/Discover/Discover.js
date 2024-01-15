e31836a2d2f359fa78efceaf53e3626054ae4ba2
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-14 22:55:57 -0500

    fix background problem in Group section

--- src/pages/Discover/Discover.js
+++ src/pages/Discover/Discover.js
@@ -3,7 +3,7 @@ import Sidebar from '../../components/Sidebar/Sidebar.js';
 
 const DiscoverPage = () => {
     return (
-        <div className="grid grid-cols-6 h-screen bg-gradient-to-tr from-yellow-600 to-purple-600">
+        <div className="grid grid-cols-6 bg-gradient-to-tr from-yellow-600 to-purple-600">
             <div className="bg-purple-500 bg-opacity-30 col-span-1">
                 <Sidebar />
             </div>
